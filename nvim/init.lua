89904900202af4dff4c4cc3624643471b39bdc6a
jlaio <mail.jlaio@gmail.com>
2025-10-08 17:32:27 +0200

    init

---
+++ nvim/init.lua
@@ -0,0 +1,52 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.undofile = true
+vim.opt.swapfile = false
+vim.opt.signcolumn = "yes"
+
+vim.pack.add({
+	{ src = "https://github.com/neovim/nvim-lspconfig" },
+	{ src = "https://github.com/mason-org/mason.nvim" },
+	{ src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+	{ src = "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ src = "https://github.com/Saghen/blink.cmp" },
+	{ src = "https://github.com/vague-theme/vague.nvim" },
+})
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-tool-installer").setup({
+	ensure_installed = {
+		"lua_ls",
+		"stylua",
+	},
+})
+
+vim.lsp.config("lua_ls", {})
+require("vague").setup({
+	-- optional configuration here
+})
+
+vim.cmd("colorscheme vague")
+require("blink.cmp").setup({
+	keymap = { preset = "default" },
+	sources = {
+		default = { "lsp", "path", "snippets", "buffer" },
+	},
+	fuzzy = { implementation = "lua" },
+	signature = { enabled = true },
+	completion = {
+		documentation = { auto_show = true, auto_show_delay_ms = 500 },
+		menu = {
+			auto_show = true,
+			draw = {
+				treesitter = { "lsp" },
+				columns = { { "kind_icon", "label", "label_description", gap = 1 }, { "kind" } },
+			},
+		},
+	},
+})
+
+vim.cmd([[autocmd BufWritePre * lua vim.lsp.buf.format()]])
